6804aab8efaac4046e5cd5bea959af6ff2e8fa17
Sebastian Laclau <sebastianlaclau@gmail.com>
2022-10-10 18:42:17 -0300

    build

---
+++ client/build/precache-manifest.0dbcfe2216702334fd83282ed68208b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f05e0170305c096169d",
+    "url": "/static/css/main.d3139505.chunk.css"
+  },
+  {
+    "revision": "4f05e0170305c096169d",
+    "url": "/static/js/main.f94d728d.chunk.js"
+  },
+  {
+    "revision": "ad49668cd40d200f7a28",
+    "url": "/static/js/runtime~main.c018a2df.js"
+  },
+  {
+    "revision": "21638f142760e31cf07b",
+    "url": "/static/css/2.1924c8ad.chunk.css"
+  },
+  {
+    "revision": "21638f142760e31cf07b",
+    "url": "/static/js/2.946e37b0.chunk.js"
+  },
+  {
+    "revision": "1666587ef87030b8fefc",
+    "url": "/static/js/3.f9617906.chunk.js"
+  },
+  {
+    "revision": "8464fbb24d3ae828a4269afad6b5d046",
+    "url": "/static/media/prisma.8464fbb2.png"
+  },
+  {
+    "revision": "3d5452c22f64cdd500f95d0a6eb2efe6",
+    "url": "/index.html"
+  }
+];
